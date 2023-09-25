a3e8de612468c7cbf500a5980c01746346318fe2
James Ratzlaff <jamesratzlaff@gmail.com>
2023-09-25 01:48:53 -0400

    (✖╭╮✖)

--- com/jamesratzlaff/ctf/saw2023/lol/ಠ_ಠ.java
+++
@@ -1,8 +0,0 @@
-package com.jamesratzlaff.ctf.saw2023.lol;
-
-public class ಠ_ಠ {
-	public ಠ_ಠ() {
-		System.out.println("Ayyyyyy...isn't funny that most people assume class names can only use Latin characters?");
-	}
-	
-}
